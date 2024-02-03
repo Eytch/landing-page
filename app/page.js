3598c1a0ee69545e92dfbfcf58acc6500c049cf7
“Ahmad <ahmad.hesham97@gmail.com>
2024-02-03 14:37:59 +0200

    added a new feature section to the Landing Page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "@/components/hero";
 import Collabs from "@/components/collabs";
 import Support from "@/components/support";
+import Features from "@/components/features";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Collabs />
       <Support />
+      <Features />
     </main>
   );
 }
---
+++ components/features.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+function Features() {
+  return (
+    <div className="max-w-[1440px] m-auto p-8">
+      <div className=" flex flex-col mb-10 lg:flex-row items-center gap-5 lg:gap-52">
+        <h3 className="text-3xl">Our Features you cab get</h3>
+        <p className="text-sm opacity-50 text-center">
+          We offer a variety of interesting features that you can help increase
+          yor productivity at work and manage your project easily.
+        </p>
+        <Button size="lg" className="rounded-full p-7">
+          Get Started
+        </Button>
+      </div>
+      <div className="flex flex-col items-center lg:justify-center lg:flex-row gap-10 lg:gap-20 ">
+        <div className="flex flex-col gap-2 max-w-xs">
+          <Image
+            className="self-center lg:self-start"
+            src="/left-image.svg"
+            width={300}
+            height={300}
+            alt=""
+          />
+          <h3 className="text-2xl text-center lg:self-start lg:text-left">
+            Collboration Teams
+          </h3>
+          <p className="text-sm self-center text-center lg:self-start opacity-50 lg:text-left">
+            Here you can handle projects together with team virtually
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 max-w-xs">
+          <Image
+            className="self-center lg:self-start"
+            src="/center-image.svg"
+            width={300}
+            height={300}
+            alt=""
+          />
+          <h3 className="text-2xl text-center lg:self-start lg:text-left">
+            Cloud Storage
+          </h3>
+          <p className="text-sm self-center text-center lg:self-start opacity-50 lg:text-left">
+            No need to worry about storage because we provide storage up to 2 TB
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 max-w-xs">
+          <Image
+            className="self-center lg:self-start"
+            src="/right-image.svg"
+            width={300}
+            height={300}
+            alt=""
+          />
+          <h3 className="text-2xl text-center lg:self-start lg:text-left">
+            Daily Analytics
+          </h3>
+          <p className="text-sm self-center text-center lg:self-start opacity-50 lg:text-left">
+            We always provide useful information to make it easier for you every
+            day
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
