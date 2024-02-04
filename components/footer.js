8eb616496b8fbb3c807e1c256cdfd76b9acd0e7d
“Ahmad <ahmad.hesham97@gmail.com>
2024-02-04 18:41:22 +0200

    added new section in footer component

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function Footer() {
   return (
@@ -34,14 +36,14 @@ function Footer() {
               alt=""
             />
           </div>
-          <div className="basis-1/3">
+          <div className="basis-1/3 lg:min-w-[450px]">
             <div className="bg-[#222938] p-4 lg:py-16 lg:px-14 rounded-lg flex flex-col">
               <div className="flex flex-col items-center gap-2">
                 <Image src="/moneyIcon.svg" width={50} height={50} alt="" />
                 <h4 className="lg:text-3xl text-white">Get Started</h4>
               </div>
               <div>
-                <form action="#" className="flex flex-col gap-3">
+                <form className="flex flex-col gap-3">
                   <div className="flex flex-col gap-2">
                     <label htmlFor="email" className="text-white">
                       Email
@@ -75,9 +77,91 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
+        <div className="flex flex-col lg:flex-row mt-20 gap-10">
+          <div className="basis-1/2 flex flex-col gap-6">
+            <div>
+              <Image
+                src="/Logo.svg"
+                alt="Biccas"
+                width={160}
+                height={30}
+                priority
+              />
+            </div>
+            <div>
+              <p className="text-white opacity-55 text-sm">
+                Get started now! Try our product
+              </p>
+            </div>
+            <div className="relative min-w-[200px] max-w-[250px] lg:min-w-[400px]">
+              <Input
+                className="w-full border-gray-300 text-white border rounded-full p-8 focus:outline-none sm:text-sm"
+                placeholder="Enter your email here"
+              />
+              <div className="absolute right-[0.3rem] top-[0.3rem] flex items-center cursor-pointer ">
+                <Button className="rounded-full w-14 h-14 z-20">
+                  <FaArrowRightLong
+                    color="white"
+                    fontSize={18}
+                    className="cursor-pointer"
+                  />
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="basis-1/2 justify-evenly align-middle flex lg:flex-row gap-2 lg:gap-5">
+            <div>
+              <ul className="flex flex-col gap-4 text-white text-sm">
+                <li>Support</li>
+                <li className="opacity-55">
+                  <Link href="#">Help center</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Account information</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">About</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Contact us</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul className="flex flex-col gap-4 text-white text-sm">
+                <li>Help and Solution</li>
+                <li className="opacity-55">
+                  <Link href="#">Talk to support</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Support docs</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">System status</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Covid responde</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul className="flex flex-col gap-4 text-white text-sm">
+                <li>Product</li>
+                <li className="opacity-55">
+                  <Link href="#">Update</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Security</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Beta test</Link>
+                </li>
+                <li className="opacity-55">
+                  <Link href="#">Pricing product</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div>
           <div></div>
